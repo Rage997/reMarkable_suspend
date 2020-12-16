41974a42d2f00910b3353b1488bb35dc5ab9e83c
Rage <zuppin@usi.ch>
2020-12-16 11:25:04 +0100

    Convert image to grayscale update logs

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 A bash script to automatically change the reMarkable suspend screen given an image.
 It will resize the image to fit the reMarkable screen. It will ask for reMarkable ssh password.
 
-# Usage
-
+## Usage
+```
 chmod 755 changeRmScreen.sh
 ./changeRmScreen.sh <input_image>
+```
--- changeRmScreen.sh
+++ changeRmScreen.sh
@@ -5,17 +5,26 @@
 # on Linux: apt install imagick
 # on Mac: brew install imagick
 
-image=$1
+# reMarkable usb ip, if reMarkable is not connect through usb write it
+rm_ip=10.11.99.1
 
-echo 'Resizing image for Remarkable (1827x1404 with 226 DPI) and saving it to: '$PWD/suspended.png  
-# TODO convert it to grayscale (add -grayscale flag)
-convert $image -resize 1404x1872! -density 226 $PWD/suspended.png
+image=$1
+if [ -z $image ]; then
+        echo "No input image! Usage: ./changeRmScreen <input_image>"
+        exit 0
+fi
+
+echo 'Resizing image for Remarkable (1827x1404 with 226 DPI)'
+convert $image -resize 1404x1872! -density 226 -colorspace Gray -quiet $PWD/suspended.png
+echo 'Done! Image saved in: '$PWD/suspended.png
 
 # echo 'Press write the Remarkable ip'
 # read rm_ip
-rm_ip=192.168.1.186
-echo 'Connecting to reMarkable throuhg ssh and storing it'
+echo 'Connecting to reMarkable through ssh and storing it'
 scp $PWD/suspended.png root@$rm_ip:/usr/share/remarkable
 
-# echo $res
-echo 'Done! Enjoy your sleep screen!'
+if [ $? -eq 0 ]; then
+    echo 'Done! Enjoy your sleep screen!'
+else
+    echo 'Something went wrong with ssh connection. Is the reMarkable on and awake?'
+fi
